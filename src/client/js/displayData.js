ec8551150a44242e063eb93d51a710ca503f6789
Abdelrazek <abdelrazekhdd@gmail.com>
2021-08-30 06:33:52 +0200

    Make sure to clear old result data before putting a new data

--- src/client/js/displayData.js
+++ src/client/js/displayData.js
@@ -1,5 +1,10 @@
 const displayData = (lists) => {
   const results = document.getElementById("results");
+
+  while (results.firstChild) {
+    results.removeChild(results.firstChild);
+  }
+
   for (const list in lists) {
     const el = document.createElement("li");
     el.textContent = `${list} : ${lists[list]}`;
